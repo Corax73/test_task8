014f5e0dcf29b8405f1aa2a0b302107b1c2ad06e
Corax73 <ramaz999@inbox.ru>
2023-07-07 22:21:01 +0300

    added lasses for connecting to the database and creating a user and checking authentication

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+require 'src/main.php';
+
---
+++ src/main.php
@@ -0,0 +1,9 @@
+<?php
+
+session_start();
+include 'config/const.php';
+
+spl_autoload_register(function ($class_name) {
+    include 'classes/' . $class_name . '.php';
+});
+

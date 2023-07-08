0e039abc1c6c3eae45134227efdd2845d8a2911a
Corax73 <ramaz999@inbox.ru>
2023-07-08 23:15:51 +0300

    minor edits, added controller and view classes, main template

--- src/main.php
+++ src/main.php
@@ -3,9 +3,10 @@
 session_start();
 include 'config/const.php';
 
-spl_autoload_register(function ($class_name) {
+/*spl_autoload_register(function ($class_name) {
     include 'models/' . $class_name . '.php';
-});
+});*/
+spl_autoload_register();
 
-$router = new Router;
+$router = new Models\Router;
 $router->run();

23021b49d663c2648afda09470f7dc343386550d
Corax73 <ramaz999@inbox.ru>
2023-07-13 22:33:05 +0300

    added autoload psr

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+//require 'vendor/autoload.php';
 
+namespace Models;
+
+require 'vendor/autoload.php';
 session_start();
 include 'config/const.php';
 
-spl_autoload_register();
+//spl_autoload_register();
 
-$router = new Models\Router;
+$router = new Router();
 $router->run();

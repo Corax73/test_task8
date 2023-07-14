cd14b710487712c15a5a804089e6a3779995ee6e
Corax73 <ramaz999@inbox.ru>
2023-07-14 22:52:51 +0300

    added phpunit and first test

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
-//require 'vendor/autoload.php';
-
 namespace Models;
 
 require 'vendor/autoload.php';
 session_start();
 include 'config/const.php';
-
-//spl_autoload_register();
 
 $router = new Router();
 $router->run();

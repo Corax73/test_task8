8e1943fc52582f91e5f854a97564ce69ad14e8a4
Corax73 <ramaz999@inbox.ru>
2023-07-15 23:09:54 +0300

    added test for InputCleaning and Router - with guzzle

---
+++ tests/RouterTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Models;
+
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+ 
+class RouterTest extends TestCase
+{
+    private $router;
+    private $http;
+ 
+    protected function setUp(): void
+    {
+        $this->router = new Router();
+        $this->http = new Client(['base_uri' => 'http://localhost:8000']);
+    }
+ 
+    protected function tearDown(): void
+    {
+        $this->router = NULL;
+        $this->http = null;
+    }
+
+    public function testCreateRouter(): void
+    {
+        $this->assertContainsOnlyInstancesOf(
+            Router::class,
+            [new Router, new InputCleaning]
+        );
+    }
+
+    public function testGet(): void
+    {
+        $response = $this->http->request('GET', '/');
+        $this->assertEquals(200, $response->getStatusCode());
+        
+        $resp = $response->getHeaders();
+        $type = $resp['Content-type'][0];
+        $this->assertEquals('text/html; charset=UTF-8', $type);
+    }
+
+    public function testContentType(): void
+    {
+        $response = $this->http->request('GET', '/');
+        $resp = $response->getHeaders();
+        $type = $resp['Content-type'][0];
+        $this->assertEquals('text/html; charset=UTF-8', $type);
+    }
+    public function testPost(): void
+    {
+        $response = $this->http->request('POST', '/create', ['username' => 'phpunit1', 'email' => 'phpunit@mail.php', 'descriptions' => 'test']);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}

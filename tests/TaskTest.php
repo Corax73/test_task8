8cc6e8be969f60ec46a56248ed12805f9a117f15
Corax73 <ramaz999@inbox.ru>
2023-07-17 09:59:23 +0300

    added more tests class Task

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -74,4 +74,55 @@ class TaskTest extends TestCase
         $result = $this->task->loadOneTaskData($this->connect, $id, $this->path);
         $this->assertCount(1, $result);
     }
+
+    public function testSaveTask(): void
+    {
+        $result = $this->task->saveTask($this->connect, 'phpunit2', 'phpunit@test.test', 'test-test', $this->path);
+        $this->assertTrue($result, $message = 'not preserved');
+    }
+
+    public function testSetImplementation(): void
+    {
+        $max = 0;
+        $result = $this->task->loadTasksData($this->connect, $this->path);
+        foreach($result as $task) {
+            if ($task['id'] > $max) {
+                $max = $task['id'];
+            }
+        }
+        $id = rand(0, $max);
+        $result = $this->task->setImplementation($this->connect, $id, 1, $this->path);
+        $this->assertTrue($result, $message = 'value not set');
+    }
+
+    public function testCountResultsLoadTasksForPaginationWithSort():void
+    {
+        $page = rand(0, $this->task->getCountPages($this->connect, $this->path));
+        $result = $this->task->loadTasksForPaginationWithSort($this->connect, $page, '`descriptions`', $this->path);
+        $this->assertCount(3, $result);
+    }
+
+    public function testReturnTypeLoadTasksForPaginationWithSort(): void
+    {
+        $page = rand(0, $this->task->getCountPages($this->connect, $this->path));
+        $result = $this->task->loadTasksForPaginationWithSort($this->connect, $page, '`descriptions`', $this->path);
+        $this->assertIsArray($result, $message = 'not array');
+    }
+
+    public function testUpdateTask(): void
+    {
+        $max = 0;
+        $result = $this->task->loadTasksData($this->connect, $this->path);
+        foreach($result as $task) {
+            if ($task['id'] > $max) {
+                $max = $task['id'];
+            }
+        }
+        $id = rand(0, $max);
+        $newData = [
+            'username' => '!phpunit3'
+        ];
+        $result = $this->task->updateTask($this->connect, $newData, $id, $this->path);
+        $this->assertTrue($result, $message = 'value not set');
+    }
 }
